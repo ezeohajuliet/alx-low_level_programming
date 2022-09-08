be2667729e497bd7b377ab253da5e8de81bdb9c7
ezeohajuliet <ezeohajuliet30@gmail.com>
2022-09-08 07:07:05 -0700

    Low Level Programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point
  *
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0)
 }
