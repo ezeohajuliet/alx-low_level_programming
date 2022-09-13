d38819ead20422137f6e928ecdbc06f3107ec19f
ezeohajuliet <ezeohajuliet30@gmail.com>
2022-09-13 13:54:46 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,9 @@
 
 /**
  * main - alphabet
- * print_alphabet_x10 - print a - z ten times 
+ * print_alphabet_x10 - prints the alphabet 10 times.
+ *
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet_x10(void)
