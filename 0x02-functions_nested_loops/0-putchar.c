5c7de21d862c3e16482d700dffc6ecae476f973f
ezeohajuliet <ezeohajuliet30@gmail.com>
2022-09-13 10:58:38 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,14 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * main - Prints Holberton as a message.
+ * main - Prints _putschar as a message.
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void main(int c)
 {
-	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
-	int count, sz;
+	_putchar(c);
 
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
 	_putchar('\n');
 	return (0);
 }
