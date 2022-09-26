5c9b2543280f6bca5208ddfeb2d729fc3c0d4d36
ezeohajuliet <ezeohajuliet30@gmail.com>
2022-09-26 08:10:34 -0700

    pointers_arrays_strings in C

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,13 +9,12 @@
  * Return: a pointer to the memory area s
  */
 
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (int i = 0; i < n; i++)
-	{
-		*(s + i) = b;
-	}
+	for (i = 0; i < n; i++)
+		*(s + i) =  b;
+
 	return (s);
 }
