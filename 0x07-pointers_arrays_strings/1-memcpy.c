10b9799dcd76ff78071ce53df1eafe8ef631dfad
ezeohajuliet <ezeohajuliet30@gmail.com>
2022-09-26 08:30:19 -0700

    pointers_arrays_strings in C

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,13 +8,11 @@
  *
  * Return: a pointer to dest
  */
-
 char *_memcpy(char *dest, char *src, unsigned int n);
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	
 		*(dest + i) = *(src + i);
 	
 	return (dest);
